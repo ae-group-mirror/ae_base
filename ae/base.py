54e65d0b9593f10a6dee7dd4e6db51a933c49d40
AndiEcker <aecker2@gmail.com>
2021-06-10 18:25:21 +0100

    V 0.1.18: prepare for new common version `2 - Pre-Alpha`.

--- ae/base.py
+++ ae/base.py
@@ -53,7 +53,7 @@ from configparser import ConfigParser, ExtendedInterpolation
 from typing import Any, AnyStr, Dict, Iterable, Optional, Tuple, cast
 
 
-__version__ = '0.1.17'
+__version__ = '0.1.18'
 
 
 BUILD_CONFIG_FILE = 'buildozer.spec'            #: app build config file
